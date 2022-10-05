086d4e0b97b77a9a0c9892f135fa4cffafe2f342
aubustou <charlemagnecontrelesninjas@gmail.com>
2022-10-06 01:12:59 +0200

    add loop

--- instable_bot/main.py
+++ instable_bot/main.py
@@ -1,6 +1,8 @@
 import logging
 import os
 import random
+import sys
+import time
 from pathlib import Path
 from typing import TypedDict
 
@@ -11,6 +13,9 @@ ACCOUNT_USERNAME = os.getenv("INSTAGRAM_USERNAME")
 ACCOUNT_PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
 
 IMAGE_PATH = Path(os.getenv("INSTAGRAM_IMAGE_PATH", "SET ME PLEASE"))
+
+WAITING_TIME = 60 * 60 * 6  # 6 hours
+JERK_OFF_TIME = 60 * 60 * 1  # 1 hour
 
 
 class PNGMetadata(TypedDict):
@@ -53,7 +58,7 @@ def upload_image(client: Client, image_path: Path, caption: str, dump_folder: Pa
         image = Image.open(image_path)
         image.save(image_path.with_suffix(".jpg"), "JPEG")
 
-    logging.info(f"Uploading {image_path} with caption {caption}")
+    logging.info("Uploading image %s with caption %s", image_path, caption)
     client.photo_upload(jpg_image_path, caption)
     jpg_image_path.rename(dump_folder / jpg_image_path.name)
 
@@ -62,31 +67,46 @@ def main():
     logging.basicConfig(level=logging.INFO)
 
     if not ACCOUNT_USERNAME or not ACCOUNT_PASSWORD:
-        print(
+        logging.critical(
             "Please set INSTAGRAM_USERNAME and INSTAGRAM_PASSWORD environment variables"
         )
-        return
+        sys.exit(1)
 
     if not IMAGE_PATH.exists():
-        print("Please set INSTAGRAM_IMAGE_PATH environment variable")
-        return
+        logging.critical("Please set INSTAGRAM_IMAGE_PATH environment variable")
+        sys.exit(2)
 
     cl = Client()
-    session_dump_file = Path("session.json")
-    if session_dump_file.exists():
-        cl.load_settings(session_dump_file)
+    try:
+        while True:
+            session_dump_file = Path("session.json")
+            if session_dump_file.exists():
+                cl.load_settings(session_dump_file)
 
-    cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
-    cl.dump_settings(session_dump_file)
+            cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
+            cl.dump_settings(session_dump_file)
 
-    already_uploaded_folder = IMAGE_PATH / "already_uploaded"
-    already_uploaded_folder.mkdir(exist_ok=True)
-    jpg_folder = IMAGE_PATH / "jpg"
-    jpg_folder.mkdir(exist_ok=True)
+            already_uploaded_folder = IMAGE_PATH / "already_uploaded"
+            already_uploaded_folder.mkdir(exist_ok=True)
+            jpg_folder = IMAGE_PATH / "jpg"
+            jpg_folder.mkdir(exist_ok=True)
 
-    image_path = random.choice(list(IMAGE_PATH.glob("*.png")))
-    upload_image(cl, image_path, generate_caption(image_path), jpg_folder)
-    image_path.rename(already_uploaded_folder / image_path.name)
+            image_path = random.choice(list(IMAGE_PATH.glob("*.png")))
+            if image_path:
+                upload_image(cl, image_path, generate_caption(image_path), jpg_folder)
+                image_path.rename(already_uploaded_folder / image_path.name)
+            else:
+                logging.warning("No images to upload")
+                break
+            cl.logout()
+
+            waiting_time = WAITING_TIME + random.randint(-JERK_OFF_TIME, 0)
+            logging.info("Waiting %.1f hours", waiting_time / 3600)
+            time.sleep(waiting_time)
+    except KeyboardInterrupt:
+        cl.logout()
+        logging.info("exiting")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
